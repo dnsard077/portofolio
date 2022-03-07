60dd25e2b8ac4b647617ec0a72fd243ea0380552
dnsard077 <danisardani077@gmail.com>
2022-03-07 21:09:20 +0700

    deploy

---
+++ precache-manifest.ec01e4238d86885ce30fe48efd2999d3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792fc88352c0218fb0fe",
+    "url": "/portofolio/css/about.aa967cdf.css"
+  },
+  {
+    "revision": "701fa0e05c240ac591cd",
+    "url": "/portofolio/css/app.c2c64dd4.css"
+  },
+  {
+    "revision": "95df4c926e1f261852af",
+    "url": "/portofolio/css/chunk-vendors.54bcddb4.css"
+  },
+  {
+    "revision": "821e22abb7b9f6ba934e",
+    "url": "/portofolio/css/project.9bb1415b.css"
+  },
+  {
+    "revision": "363b8d060ca0bac2fedb",
+    "url": "/portofolio/css/project2.4fa3de44.css"
+  },
+  {
+    "revision": "ac16cb5d6efee32aa3f742f3789f0886",
+    "url": "/portofolio/img/avatar.ac16cb5d.png"
+  },
+  {
+    "revision": "f41b010b7f41777f7721285079a5162e",
+    "url": "/portofolio/img/code.f41b010b.png"
+  },
+  {
+    "revision": "7f2e92a0f322b9692182ca02538d3ab3",
+    "url": "/portofolio/img/coding.7f2e92a0.png"
+  },
+  {
+    "revision": "7aa0824b0da177f600effae3b3828478",
+    "url": "/portofolio/img/plant.7aa0824b.png"
+  },
+  {
+    "revision": "59dfbb92d120a0e9009c27c3a9922dd9",
+    "url": "/portofolio/img/project1.59dfbb92.jpg"
+  },
+  {
+    "revision": "3eaef003df29ff9871e9e089fdb673a0",
+    "url": "/portofolio/img/project2.3eaef003.png"
+  },
+  {
+    "revision": "e5bc52d7d6dd84591501f914060704c6",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "792fc88352c0218fb0fe",
+    "url": "/portofolio/js/about.868a3793.js"
+  },
+  {
+    "revision": "701fa0e05c240ac591cd",
+    "url": "/portofolio/js/app.f14ab70f.js"
+  },
+  {
+    "revision": "95df4c926e1f261852af",
+    "url": "/portofolio/js/chunk-vendors.6db749a6.js"
+  },
+  {
+    "revision": "821e22abb7b9f6ba934e",
+    "url": "/portofolio/js/project.91147745.js"
+  },
+  {
+    "revision": "363b8d060ca0bac2fedb",
+    "url": "/portofolio/js/project2.8fa5dfde.js"
+  },
+  {
+    "revision": "e97c5160991cd3ef67cb4ebb1761dde9",
+    "url": "/portofolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portofolio/robots.txt"
+  }
+]);
